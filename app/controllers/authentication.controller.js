0ace6b5d542a2b606190f21334e4bb8e3aa8b0d4
Diego Haro Vivanco <diego.harovivanco@gmail.com>
2023-07-07 22:07:32 -0300

    token para usuario loggeado y se arreglaron algunos fix

--- app/controllers/authentication.controller.js
+++ app/controllers/authentication.controller.js
@@ -1,4 +1,8 @@
 import bcrypt from "bcryptjs";
+import jsonWebToken from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const usuarios = [{
     user: "lio",
@@ -10,43 +14,64 @@ const usuarios = [{
 const login = async (req, res) =>{
     console.log(req.body)
 
-    const user = req.body.user;
-    const password = req.body.password;
-    if(!user || !password){
-        return res.status(400).send({status:"Error", message: "Campos sin valores"})
-    }
-    const userRevisado = usuarios.find(usuario =>  usuario.user === user);
-    if(!userRevisado){
-       return res.status(400).send({status:"Error", message: "Usuario y/o contraseña incorrectos"})
-    }
-    const loginCorrecto = await bcrypt.compare(password, userRevisado)
-    
+    try {
+        console.log(req.body)
 
+        const user = req.body.user;
+        const password = req.body.password;
+        if(!user || !password)
+            return res.status(400).send({status:"Error", message: "Campos sin valores"})
+        
+        const userRevisado = usuarios.find(usuario =>  usuario.user === user);
+        if(!userRevisado)
+           return res.status(401).send({status:"Error", message: "Usuario y/o contraseña incorrectos"})
+        
+        const loginCorrecto = await bcrypt.compare(password, userRevisado.password)
+        console.log(loginCorrecto)
+        if(loginCorrecto){
+    
+            // Si el login es correcto, generamos un token
+            const token = jsonWebToken.sign(
+                {user:userRevisado.user},
+                process.env.JWT_SECRET, {expiresIn:process.env.JWT_EXPIRED})
+        
+            const cookieOption = {
+                expires: new Date(Date.now()+ process.env.JWT_COOKIE_EXPIRED * 24*60*1000), // lo convierto en días
+                path: "/"
+            }
+            res.cookie("jwt",token, cookieOption)
+            return res.send({status:"ok", message: "Usuario loggeado correctamente", redirect:"./administrador"})   
+        }
+        return res.status(401).send({status:"Error", message: "Usuario y/o contraseña incorrectos"})
+    
+    } catch (error) {
+        res.status(400).send({status:"Error",message:error});
+    }
 
 }
 
 
 const registro = async (req, res) =>{
-    //console.log(req.body)
+
     const user = req.body.user;
     const password = req.body.password;
     const email = req.body.email;
-    if(!user || !password || !email){
-        return res.status(400).send({status:"Error", message: "Campos sin valores"})
-    }
+    if(!user || !password || !email)
+        return res.status(400).send({status:"Error", message: "Campos incompletos"})
+    
     const userRevisado = usuarios.find(usuario =>  usuario.user === user);
-    if(userRevisado){
-       return res.status(400).send({status:"Error", message: "El usuario ingresado ya existe en el sistema"})
+    if(userRevisado)
+       return res.status(301).send({status:"Error", message: "El usuario ingresado ya existe en el sistema"})
 
-    }
+    
     const salt = await bcrypt.genSalt(5); // 5 procesos de "salt" antes de encriptar la contraseña
     const hashPassword = await bcrypt.hash(password, salt)
     const nuevoUsuario = {
         user, email, password: hashPassword
     }
-   // console.log(nuevoUsuario)
     usuarios.push(nuevoUsuario);
-    //console.log(usuarios)
+    console.log(usuarios)
+
     return res.status(201).send({status: "ok", message:"usuario agregado", redirect: "/"})
 
 
--- app/public/login.js
+++ app/public/login.js
@@ -3,21 +3,21 @@ const msjErorr = document.getElementsByClassName("error")[0];
 document.getElementById("login").addEventListener("submit", async(e)=>{
     e.preventDefault();
 
-    const user = e.target.children.usuario.value;
-    const password = e.target.children.password.value;
 
-    const response = await fetch("http://localhost:8080/api/login",{
+
+    const res = await fetch("http://localhost:8080/api/login",{
         method: "POST",
         headers:{
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            user,password
+            user : e.target.children.usuario.value,
+            password : e.target.children.password.value
             
         })
-    })
-    if(!response.ok) return msjErorr.classList.toggle("escondido", false);
-    const resJson = await response.json();
+    });
+    if(!res.ok) return msjErorr.classList.toggle("escondido", false);
+    const resJson = await res.json();
     if(resJson.redirect){
         window.location.href = resJson.redirect;
     }
